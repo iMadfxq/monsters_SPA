b98bc8a924583067740976b776191519d524d6e5
iMadfxq <imadfxq@gmail.com>
2022-04-01 23:48:55 -0500

    Started implementing search bar

--- src/App.js
+++ src/App.js
@@ -11,14 +11,34 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(data => data.json())
-      .then((users) => this.setState(() => {return {monsters: users}}, () => console.log(this.state)))
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((data) => data.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => console.log(this.state)
+        )
+      );
   }
 
   render() {
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search monsters"
+          onChange={(event) => {
+            let searchedString = event.target.value.toLocaleLowerCase()
+            const filteredMonsters = this.state.monsters.filter(
+              (monster) => {return monster.name.toLocaleLowerCase().includes(searchedString)}
+            );
+
+            this.setState(() => {return {monsters : filteredMonsters}})
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>;
         })}
